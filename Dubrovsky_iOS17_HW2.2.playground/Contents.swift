6e42ef7843af2aec4d9152e7eec85144ee6f6b84
iDubrovsky <iapopko1@mts.ru>
2021-10-21 22:20:50 +0300

    [5I] Реализовал класс Track в 1-ом задании.

--- Dubrovsky_iOS17_HW2.2.playground/Contents.swift
+++ Dubrovsky_iOS17_HW2.2.playground/Contents.swift
@@ -1,3 +1,6 @@
+import Foundation
+
+
 //  MARK: - Задача #1.
 /*  Вы разрабатываете библиотеку аудиотреков. Вам необходимо реализовать одну из категорий музыки, наполненную треками.
     Алгоритм выполнения:
@@ -7,6 +10,24 @@
     - Объявите в нем свойства название категории, список треков и количество треков в списке (экспериментируйте с "ленивыми" и вычисляемыми свойствами).
     - Определите методы добавления и удаления треков в эту категорию.*/
 
+
+class Track {
+    private let artist: String
+    private let nameSong: String
+    private let country: String
+    private let duration: String
+    var fullDescriptionSong: String { "Artist: \(artist), Name: \(nameSong), Country: \(country), Duration: \(duration)." }
+    
+    init(artist: String, nameSong: String, country: String, duration: String) {
+        self.artist = artist
+        self.nameSong = nameSong
+        self.country = country
+        self.duration = duration
+    }
+}
+
+var one = Track(artist: "A", nameSong: "B", country: "C", duration: "D")
+print(one.fullDescriptionSong)
 
 
 
@@ -44,3 +65,99 @@
 
 //  MARK: - Задача #3.
 //  Преобразуйте классы так, чтобы в пределах вашей библиотеки можно было обмениваться треками между категориями.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Tracker {
+    private var timer: Timer?
+    var interval: TimeInterval? {
+        willSet {
+            if let _ = interval { print("Timer will be started with interval: \(interval).") }
+            guard let _ = timer else { return }
+            print("Timer stopped.")
+            timer?.invalidate()
+            timer = nil
+        }
+        didSet {
+            if let _ = oldValue {
+                print("Old interval was: \(oldValue).")
+            } else {
+                print("No interval beforeL: \(oldValue).")
+            }
+            
+            guard let newInterval = interval else { return }
+            timer = Timer.scheduledTimer(withTimeInterval: newInterval,
+                                         repeats: true,
+                                         block: { _ in print("Do  will be started with interval: \(newInterval).")} )
+        }
+    }
+}
+
+
+let tracker = Tracker()
+tracker.interval = 3
+tracker.interval = 4
+
+DispatchQueue.main.asyncAfter(deadline: .now() + 10) {
+    tracker.interval = 2
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
