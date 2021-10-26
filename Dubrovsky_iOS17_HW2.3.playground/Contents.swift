a055196019e019f10ba1071c91b3ae3fc955a57c
iDubrovsky <iapopko1@mts.ru>
2021-10-26 10:52:03 +0300

    [6I] Реализовал 3-е задание.Э

--- Dubrovsky_iOS17_HW2.3.playground/Contents.swift
+++ Dubrovsky_iOS17_HW2.3.playground/Contents.swift
@@ -63,6 +63,7 @@ flightToEurope_1() { $0 * $1 / 40}
 print("------------------------------------------------")
 flightToEurope_2(satelliteWeight: 200, flightLength: 30_000) { $0 / $1 * 100}
 flightToEurope_2(satelliteWeight: 200, flightLength: 30_000) { $0 / $1 * 100}
+print("------------------------------------------------\n")
 
 
 
@@ -87,135 +88,8 @@ flightToEurope_2(satelliteWeight: 200, flightLength: 30_000) { $0 / $1 * 100}
     }*/
 
 
+print("ЗАДАЧА #3.")
+flightToEurope_1() { $0 * $1 / 10}
 
-
-
-
-
-
-
-
-
-
-
-// MARK: - Код с лекции.
-
-typealias SomeWork = (String, String) -> Void
-let doSomeWork: SomeWork = { (nameOne: String, nameTwo: String) -> Void in
-    print("First name: \(nameOne), nameTwo: \(nameTwo).")
-}
-
-let unsortedNumners = [1, 5, 6, 12, 4, 10, 5, 2, 1, 44, 56, 130, 1550]
-
-func filter(numbers: [Int], closure: (Int) -> Bool) -> [Int] {
-    var result: [Int] = []
-    for number in numbers {
-        if closure(number) {
-            result.append(number)
-        }
-    }
-    return result
-}
-
-//func isEven(number: Int) -> Bool {
-//    return number % 2 == 0
-//}
-
-func isMouleOfTen(number: Int) -> Bool {
-    return number % 10 == 0
-}
-
-let isEven = { (number: Int) -> Bool in
-    return number % 2 == 0
-}
-
-let result = filter(numbers: unsortedNumners) { $0 % 5 == 0 }
-print(result)
-
-
-
-
-// В данном случае, компалятор посчитает сразу first, потом second, и потом зайдет в функцию
-func isValid(first: Bool, second: Bool) -> Bool {
-    return first && second
-}
-
-if isValid(first: 5 > 2, second: [1, 2, 3, 4, 5].count > 2) {
-    print("IsValue true")
-}
-
-// Сдесь создана оптимизация за счёт замыкания, если first будет 0, то second высчитываться не будет.
-// Т.к. сначала высчитается first, потом перейдет во внутрь, если first будет true, то начнется высчитывание замыкания.
-func isValid_2(first: Bool, second: () -> Bool) -> Bool {
-    return first && second()
-}
-
-if isValid_2(first: 5 > 2, second: { [1, 2, 3, 4, 5].count > 2 }) {
-    print("IsValue_2 true")
-}
-
-// Если ставить @autoclosure, то фигурные скобки можно не ставить (компилятор сам поставит).
-// Но аргумент second всё равно останется замыканием.
-func isValid_3(first: Bool, second: @autoclosure () -> Bool) -> Bool {
-    return first && second()
-}
-
-if isValid_3(first: 5 > 2, second: [1, 2, 3, 4, 5].count > 2) {
-    print("IsValue_3 true")
-}
-
-
-
-
-class Boss {
-    private let worker: Worker
-    
-    init(worker: Worker) {
-        self.worker = worker
-    }
-    
-    // При такой реализации (указания self.worker), классы будут ссылаться друг на друга.
-    // Потому что у них друг на друга сильный ссылки, и сами они разорваться не могут.
-    func start_1() {
-        worker.completion = {
-            print("Great worker \(self.worker.name). Pay money.")
-        }
-        worker.doSomeWork()
-    }
-    
-    // Чтобы избежать этого, нужно использовать слабую ссылку
-    // Слабая ссылка позволяет нам ссылаться на self, но счетчик ссылок увеличиваться не будет.
-    // В таком случае, self внутри будет опциональным
-    func start() {
-        worker.completion = { [weak self] in   // Если написать [self] in, то это будет сильной ссылкой
-            guard let sSelf = self else { return }
-            print("Great worker \(sSelf.worker.name). Pay money.")
-        }
-        worker.doSomeWork()
-    }
-    
-    deinit {
-        print("Boss will be removed from memory.")
-    }
-}
-
-class Worker {
-    let name: String
-    var completion: (() -> Void)?
-    
-    init(name: String) {
-        self.name = name
-    }
-    
-    func doSomeWork() {
-        print("Do something.")
-        // Long work ...
-        completion?()   // Что значит ? перед скобками. Если completion не nil, то он вызовется.
-    }
-}
-
-var worker: Worker? = Worker(name: "Josh")
-var boss: Boss? = Boss(worker: worker!)
-boss?.start()
-boss = nil
-worker = nil
+/*  Объясненение 2-го задания:
+    Ключевое слово параметра - "param", убрано потому, что оно является замыканием, является последним и единственных входящим зымыканием, поэтому мы можем его вынести за круглые скобки, и написать реализацию closure внутри {}. */
